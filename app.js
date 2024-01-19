478437f1b9a903967d0713d2d4f4efd397453d9c
AndreaCampo <andreacampog@gmail.com>
2024-01-19 10:53:00 -0500

    verificar-intento-usuario

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-//titulo es de tipo objeto
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del número secreto";
+
+//Haciendolo de forma directa
+/*let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Indica un número del 1 al 10";*/
 
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Indica un número del 1 al 10";
+let numeroSecreto =generarNumeroSecreto();
+console.log(numeroSecreto);
 
-//Funcion que realiza la actividad al presionar el boton Intentar 
-function intentoDeUsuario(){
-    alert('Click desde el boton');
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;  
 }
+
+//Funcion que realiza la actividad al presionar el boton Intentar funciona 
+//desde el html porque esta en un onclick 
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);  
+    //console.log(typeof(numeroDeUsuario));
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario === numeroSecreto);
+    //puedo usar tres iguales porque compara igualdad en valor y tipo de dato
+    
+}
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+}
+
+//Haciendolo mediante el uso de una función 
+asignarTextoElemento('h1','Juego del número secreto');
+asignarTextoElemento('p','Indica un número del 1 al 100');
