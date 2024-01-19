c4713da409ca04d1bf2081f1d21bc4e6047a552b
AndreaCampo <andreacampog@gmail.com>
2024-01-19 09:49:42 -0500

    uso-de-funciones-js

---
+++ app.js
@@ -0,0 +1,12 @@
+//titulo es de tipo objeto
+let titulo = document.querySelector('h1');
+titulo.innerHTML = "Juego del número secreto";
+
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Indica un número del 1 al 10";
+
+//Funcion que realiza la actividad al presionar el boton Intentar 
+function intentoDeUsuario(){
+    alert('Click desde el boton');
+}
