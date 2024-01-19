24b3c18de9dfc9139d647cfda2e08ffe5ae95d3b
AndreaCampo <andreacampog@gmail.com>
2024-01-19 13:43:35 -0500

    funcionalidad-nuevo-juego-limpiar-caja-

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 parrafo.innerHTML = "Indica un número del 1 al 10";*/
 
 
-let numeroSecreto =generarNumeroSecreto();
-console.log(numeroSecreto);
+let numeroSecreto = 0;
+let intentos = 0;
 
 function asignarTextoElemento(elemento,texto){
     let elementoHTML = document.querySelector(elemento);
@@ -15,18 +15,46 @@ function asignarTextoElemento(elemento,texto){
 //Funcion que realiza la actividad al presionar el boton Intentar funciona 
 //desde el html porque esta en un onclick 
 function verificarIntento(){
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);  
-    //console.log(typeof(numeroDeUsuario));
-    console.log(numeroDeUsuario);
-    console.log(numeroDeUsuario === numeroSecreto);
-    //puedo usar tres iguales porque compara igualdad en valor y tipo de dato
-    
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);      
+    //console.log(numeroDeUsuario);
+    if(numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1)? 'vez':'veces' }`);        
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if(numeroDeUsuario > numeroSecreto){
+            asignarTextoElemento('p', 'El número secreto es menor')
+        }else{
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;   
+        limpiarCaja();     
+    }
+    return;
 }
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+}
+
 
 function generarNumeroSecreto(){
     return Math.floor(Math.random()*10)+1;
 }
 
-//Haciendolo mediante el uso de una función 
-asignarTextoElemento('h1','Juego del número secreto');
-asignarTextoElemento('p','Indica un número del 1 al 100');
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto');
+    asignarTextoElemento('p','Indica un número del 1 al 10');    
+    numeroSecreto =generarNumeroSecreto();
+    console.log(numeroSecreto);
+    intentos = 1;    
+    document.querySelector('#reiniciar').setAttribute('disabled',true); //diferente al remove
+    //para este caso debo agregar el atributo y setearlo en true
+}
+
+function reiniciarJuego(){
+    limpiarCaja();
+    condicionesIniciales();
+}
+
+condicionesIniciales();
+
